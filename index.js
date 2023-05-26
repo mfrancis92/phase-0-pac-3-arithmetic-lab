fd7dbe6ddc4ce5b8735fadc6762470776903da67
Montell Francis <montelldfrancis@gmail.com>
2023-05-26 02:00:36 -0400

    complete assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n += 1;
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10); // turn "2" to 2 (string -> integer)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
